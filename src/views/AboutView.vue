76afa984d4e014c1ae6c2cc004400e22c44be4cc
alitarshishi <ali.tar.22343@gmail.com>
2023-07-23 16:19:18 +0300

    new

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur, dolorem id molestias mollitia natus perspiciatis possimus recusandae sit. Accusantium commodi cupiditate debitis dolore eum ex molestias mollitia, nisi placeat veniam.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur, dolorem id molestias mollitia natus perspiciatis possimus recusandae sit. Accusantium commodi cupiditate debitis dolore eum ex molestias mollitia, nisi placeat veniam.</p>
+
   </div>
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,19 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1> Homepage</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur, dolorem id molestias mollitia natus perspiciatis possimus recusandae sit. Accusantium commodi cupiditate debitis dolore eum ex molestias mollitia, nisi placeat veniam.</p>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur, dolorem id molestias mollitia natus perspiciatis possimus recusandae sit. Accusantium commodi cupiditate debitis dolore eum ex molestias mollitia, nisi placeat veniam.</p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
 
 export default {
   name: 'HomeView',
   components: {
-    HelloWorld
+
   }
 }
 </script>
