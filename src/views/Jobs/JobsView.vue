2bc511bc3c45cf761e444cb26eadfd70aa55b9df
alitarshishi <ali.tar.22343@gmail.com>
2023-07-23 16:12:58 +0300

    new

---
+++ src/views/Jobs/JobsView.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1>Jobs</h1>
+  <div v-for="job in jobs " :key="job.id">
+  <h2>{{job.title}}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return{
+      jobs:[
+        {title:'Ninja UX Designer',id:1,details:'lrem'},
+        {title:'Ninja web Designer',id:2,details:'lrem'},
+        {title:'Ninja vue Designer',id:3,details:'lrem'},
+      ]
+    }
+  }
+}
+</script>
+
+
+<style>
+
+</style>
